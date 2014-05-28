862e31d50a4b2210b2ed8d98d6b1009e36e44afc
yangyu <yundkyy@gmail.com>
2014-05-28 20:04:19 +0800

    bug

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-
+var port = (process.env.VMC_APP_PORT || 3000);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
 app.get('/', function(req, res){
   res.send('hello world yangyu');
 });
 
-app.listen(3000);
+app.listen(port,host);
