c62f6b9f3f76d32d5bc794da740094a6c11f8664
yangyu <yundkyy@gmail.com>
2014-05-28 19:50:20 +0800

    bug修改

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
- var port = (process.env.VMC_APP_PORT || 3000);
- var host = (process.env.VCAP_APP_HOST || 'localhost');
- var http = require('express');
- var app = http.createServer();
- app.get('/', function(req, res) {
-    res.send('Hello from MoPaaS');
-    });
- app.listen(port, host);
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('hello world yangyu');
+});
+
+app.listen(3000);
